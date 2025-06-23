aaa08902a4fe43287363b38a8d42bd42546c3e4a
irfan7626 <63176761+irfan7626@users.noreply.github.com>
2025-06-23 16:31:53 +0700

    Create PlayerMovement.cs

---
+++ Script/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    private Rigidbody rb;
+    private bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveX, 0f, moveZ) * moveSpeed;
+        Vector3 newVelocity = new Vector3(move.x, rb.velocity.y, move.z);
+        rb.velocity = newVelocity;
+
+        // Tombol lompat
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    // Cek apakah menyentuh tanah
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
